cae682012566df9db49804002b15de92a108b5ed
Brad Heller <brad.heller@gmail.com>
2023-04-12 16:30:49 +0000

    Updated example.

--- hello_world/app.py
+++ hello_world/app.py
@@ -3,9 +3,11 @@ import tower
 @tower.setup
 def hello_world_setup():
     print("Calling setup")
+    return "Setup."
 
 @tower.inference
 def hello_world_inference():
     print("Calling inference")
+    return "Hello, world!"
 
 tower.start()
